151ca7e06976677e300d8c16fe4c5addabbe63c0
Nodirbek <berdimurodovnodir89@gmail.com>
2025-12-26 15:05:43 +0500

    bajarildi

--- task35.py
+++ task35.py
@@ -1,7 +1,18 @@
-text = ['salom']
-if len(text) <= 3:
-    print('Harfli')
-elif 4 < len(text) < 6:
-    print('Harfli')
-elif len(text) > 6:
-    print(text)
+texts = ["kitob", "dastur", "AI", "hello", "python"]
+
+groups = [
+    [],
+    [],
+    []
+]
+
+for i in texts:
+    n = len(i)
+    if n <= 3:
+        groups[0].append(i)
+    elif 4 <= n <= 6:
+        groups[1].append(i)
+    else:
+        groups[2].append(i)
+
+print(groups)
